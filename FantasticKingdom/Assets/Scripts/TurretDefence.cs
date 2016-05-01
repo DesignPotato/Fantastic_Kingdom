c057b821a4cfb2746b61565c761f09124ede498c
Kelvin Shen <kelvinshen@outlook.com>
2016-05-01 23:06:21 +1200

    turret tower archery 50%

---
+++ FantasticKingdom/Assets/Scripts/TurretDefence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TurretDefence : MonoBehaviour {
+    public GameObject myPrefab;
+
+    private Collider[] _targets;
+    private DateTime _lastShotTime;
+    private int _shotsIntervalInSeconds = 3;
+    private int _firePowerRadius = 25;
+    private Vector3 _arrowSourcePos;
+
+    // Use this for initialization
+    void Start () {
+        // Set weapon range
+        var turretBaseCentre = new Vector3(transform.position.x, 0, transform.position.z);
+        _targets = Physics.OverlapSphere(turretBaseCentre, _firePowerRadius);
+
+        // Set shoot start point
+        _arrowSourcePos = new Vector3(transform.position.x, 10, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_targets.Length > 0 && _lastShotTime.AddSeconds(_shotsIntervalInSeconds) <= DateTime.Now)
+        {
+            _lastShotTime = DateTime.Now;
+            // Shoot at the 1st target
+            var myArrow = (GameObject)Instantiate(myPrefab, _arrowSourcePos, Quaternion.Euler(0, 0, 130));
+            Debug.Log("Turret Fire!");
+        }
+	}
+}
---
+++ FantasticKingdom/Assets/Scripts/Weapon/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 direction = new Vector3(-5.0f, -5.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(direction, ForceMode.Impulse);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        //Debug.Log("Arrow Update");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Arrow hit");
+        GameObject.Destroy(this.gameObject);
+    }
+}
