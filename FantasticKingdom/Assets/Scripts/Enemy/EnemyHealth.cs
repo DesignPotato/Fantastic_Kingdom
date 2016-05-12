6a438264e65881f17b1602f6ad795ffa3ce6aecb
ZMacfarlane <zachmacfarlane@live.com>
2016-05-13 08:50:53 +1200

    Added skeleton player attack/enemy recieve damage

---
+++ FantasticKingdom/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth;
+    public int currentHealth;
+
+    Animator anim;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (isDead)
+            return;
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+    }
+}
---
+++ FantasticKingdom/Assets/Scripts/Enemy/FollowScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScript : MonoBehaviour {
+
+    public Transform followTarget;
+    public Animator anim;
+
+    NavMeshAgent nma;
+
+
+	void Awake () {
+        nma = GetComponent<NavMeshAgent>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Follow();
+    }
+
+    void Follow()
+    {
+        anim.SetBool("IsWalking", true);
+        nma.destination = followTarget.position;
+    }
+}
