3f0d316e53ad3dfd77e43495c3bd2752a71e21c9
Ben Remnant <ben.remnant@gmail.com>
2016-05-20 15:29:58 +1200

    Added start menu

---
+++ FantasticKingdom/Assets/Scripts/Managers/StartManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class StartManager : MonoBehaviour {
+
+	public GameObject quitCheck;
+	public Texture2D cursorTexture;
+
+	public void Start(){
+		Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+	}
+
+	public void checkQuit(bool enabled){
+		quitCheck.SetActive (enabled);
+	}
+
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
