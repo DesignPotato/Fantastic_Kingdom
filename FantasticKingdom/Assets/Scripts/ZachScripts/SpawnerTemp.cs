b2f0c7429c57c5edb68571967d7070ecd47600fe
ZMacfarlane <zachmacfarlane@live.com>
2016-05-20 16:24:16 +1200

    Replaced missing scripts

---
+++ FantasticKingdom/Assets/Scripts/ZachScripts/SpawnerTemp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerTemp : MonoBehaviour {
+
+    public GameObject goblin;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        time = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        
+        if(time <= 0)
+        {
+            //GameObject newGoblin = (GameObject)Instantiate(Resources.Load("Goblin"), transform.position, transform.rotation);
+            //newGoblin.gameObject.SetActive(true);
+            Instantiate(goblin, transform.position, transform.rotation);
+            time = 3f;
+        }
+    }
+}
