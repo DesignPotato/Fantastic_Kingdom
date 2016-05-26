ddfffb9baa63cb68caf112be17afdfd50d5973b9
godfreya <ant.godfrey1@gmail.com>
2016-05-27 07:48:24 +1200

    Goblins now spawn in waves

--- FantasticKingdom/Assets/Scripts/Spawner.cs
+++ FantasticKingdom/Assets/Scripts/Spawner.cs
@@ -4,25 +4,50 @@ using System.Collections.Generic;
 public class Spawner : MonoBehaviour {
 
     private List<Transform> spawnPoints;
+    public int SpawnTime = 10;
+    private float spawnTimer;
+    private int wave;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         spawnPoints = new List<Transform>();
         foreach(GameObject g in GameObject.FindGameObjectsWithTag("SpawnPoint"))
         {
             spawnPoints.Add(g.transform);
-            GameObject newGoblin = (GameObject)Instantiate(Resources.Load("Goblin"), g.transform.position, g.transform.rotation);
-            newGoblin.gameObject.SetActive(true);
         }
+        spawnTimer = SpawnTime;
+        wave = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        if(false)
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
         {
-            GameObject newGoblin = (GameObject)Instantiate(Resources.Load("Goblin"), transform.position, transform.rotation);
-            newGoblin.gameObject.SetActive(true);
+            spawn();
+            spawnTimer = SpawnTime;
+            wave++;
+        }
+    }
+
+    private void spawn()
+    {
+        List<int> spawnNums = new List<int>();
+        for(int i = 0; i < spawnPoints.Count; i++)
+        {
+            spawnNums.Add(i);
+        }
+        for(int j = 0; j < wave; j++)
+        {
+            int x = Random.Range(0, spawnNums.Count);
+            int point = spawnNums[x];
+            spawnNums.RemoveAt(x);
+            for(int y = 0; y < 5; y++)
+            {
+                GameObject newGoblin = (GameObject)Instantiate(Resources.Load("Goblin"), spawnPoints[point].position, spawnPoints[point].rotation);
+                newGoblin.gameObject.SetActive(true);
+            }
         }
     }
 }
