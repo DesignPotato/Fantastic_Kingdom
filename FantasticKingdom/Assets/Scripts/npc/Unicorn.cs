e7b1cd5fb3c52be9e47b86912609750fc0423ad2
godfreya <ant.godfrey1@gmail.com>
2016-06-09 19:33:11 +1200

    Added some animation to the unicorn

---
+++ FantasticKingdom/Assets/Scripts/npc/Unicorn.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unicorn : Unit {
+
+    public const int GOLDSTEAL = 25;
+    public GameObject GoldPile;
+    private int goldStolen = 0;
+
+    private Animator anim;
+
+    static float HEALTHSTAT = 25f;
+    static int ARMOURSTAT = 5;
+    static int MAGSTAT = 5;
+    static int DAMAGESTAT = 10;
+
+    // Use this for initialization
+    public override void Start()
+    {
+        GoldPile = GameObject.Find("GoldPile");
+
+        //Some initial values
+        health = HEALTHSTAT;
+        armour = ARMOURSTAT;
+        magResist = MAGSTAT;
+        damage = DAMAGESTAT;
+        speed = 4;
+
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        //agent.destination = GoldPile.transform.position;
+        anim.SetBool("Attacking", true);
+    }
+
+    public static void upStats(float modifier)
+    {
+        HEALTHSTAT = HEALTHSTAT * modifier;
+        ARMOURSTAT = (int)(ARMOURSTAT * modifier);
+        MAGSTAT = (int)(MAGSTAT * modifier);
+        DAMAGESTAT = (int)(DAMAGESTAT * modifier);
+    }
+
+    // Update is called once per frame
+    public override void Update () {
+	
+	}
+}
